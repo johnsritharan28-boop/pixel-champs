1cd0dd305bb569a15c226ebc711be5da01c42821
johnsritharan28-boop <johnsritharan28@gmail.com>
2026-09-16 20:33:13 -0700

    V79 add offline app shell

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE = 'pixel-champs-v79';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './Pixel_Champs_V77_Playable_Expansion.html'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
+});
